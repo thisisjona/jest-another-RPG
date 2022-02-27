35ed4e5a29841a4e441156b801e22dba0b5c3b70
thisisjona <thisisjona@gmail.com>
2022-02-27 17:30:17 -0600

    created prototype methods

--- __tests__/Player.test.js
+++ __tests__/Player.test.js
@@ -6,7 +6,7 @@ console.log(new Potion());
 
 const Player = require('../lib/Player');
 
-test('creates a player object', () => {
+test("creates a player object", () => {
     const player = new Player ('Dave');
 
     expect(player.name).toBe('Dave');
@@ -17,4 +17,21 @@ test('creates a player object', () => {
     );
 });
 
+test ("gets a player's stats as an ojbect", () => {
+    const player = new Player('Dave');
 
+    expect(player.getStats()).toHaveProperty('potions');
+    expect(player.getStats()).toHaveProperty('health');
+    expect(player.getStats()).toHaveProperty('strength');
+    expect(player.getStats()).toHaveProperty('agility');
+});
+
+test("gets inventory from player or returns false", () => {
+    const player = new Player("Dave");
+
+    expect(player.getInventory()).toEqual(expect.any(Array));
+
+    player.inventory = [];
+
+    expect(player.getInventory()).toEqual(false);
+})
